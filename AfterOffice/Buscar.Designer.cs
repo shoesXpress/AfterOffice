216f6be366f2b8c4d299c3d47bec55deece88337
shoesXpress <jmaxqs@gmail.com>
2015-05-26 16:20:11 -0300

    cambio color letra
    cambio color

--- AfterOffice/Buscar.Designer.cs
+++ AfterOffice/Buscar.Designer.cs
@@ -118,6 +118,7 @@
             // 
             // button1
             // 
+            this.button1.ForeColor = System.Drawing.SystemColors.HotTrack;
             this.button1.Location = new System.Drawing.Point(106, 190);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(127, 44);
